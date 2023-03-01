d5aed44bf442eb1283a737b5ffc01b496d1f978a
JosephThuku <jthuku490@gmail.com>
2023-03-01 09:46:42 -0800

    fixed betty space

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -10,6 +10,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	func(tree->n);
-	binary_tree_preorder(tree->left,func);
-	binary_tree_preorder(tree->right,func);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
